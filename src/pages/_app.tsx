a19969807924f23597f93766e6822b326b0bb23f
kondrse <kondor85@mail.ru>
2026-01-14 22:18:27 +0300

    add tailwind css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,10 @@ import './globals.css';
 export default function HomePage() {
   return (
     <div className="">
-      <h1 className="text-pink-700">Welcome to the Edit Route Image Microfrontend</h1>
-      <p>This is the home page of the shop app, running on its own.</p>
+      <div className="text-center p-5">
+        <h1 className="text-2xl text-pink-700">Welcome to the Edit Route Image Microfrontend</h1>
+        <p>This is the home page of the shop app, running on its own.</p>
+      </div>
       <EditImage
         imgSrc=""
         name="Воин света"
